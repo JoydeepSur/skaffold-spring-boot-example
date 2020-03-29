0488c02498265e421c8fe6b45706efa60423c5db
Joydeep Sur <joydeepsur6@gmail.com>
2020-03-29 23:33:24 +0530

    initial commit

---
+++ src/main/java/com/example/Skaffoldspringbootexample/SkaffoldSpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Skaffoldspringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaffoldSpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkaffoldSpringBootExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Skaffoldspringbootexample/SkaffoldSpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Skaffoldspringbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaffoldSpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
